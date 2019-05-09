502ee83ce7be145a3557960d8c93d7941e3b7e08
Peter Mercado <peter.mercado255@gmail.com>
2019-05-09 16:40:43 -0400

    seration of controlers

--- EAEW3D/Assets/Scripts/Movement/Mover.cs
+++ EAEW3D/Assets/Scripts/Movement/Mover.cs
@@ -5,32 +5,23 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] Transform target;
 
     void Update()
     {
-        if (Input.GetMouseButton(0)){
-            MoveToCursor();
-        }
-
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-
-        if (hasHit){
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }    
+        GetComponent<NavMeshAgent>().destination = destination;
     }
-    private void UpdateAnimator(){
+
+    private void UpdateAnimator()
+    {
         Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
         Vector3 localVelocity = transform.InverseTransformDirection(velocity);
         float speed = localVelocity.z;
         GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-
-}
-
+    }
 }
