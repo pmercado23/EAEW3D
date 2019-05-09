8b102dc3a40a5a1a6d7fb3d20fb3f6dc7f402d8f
unknown <peter.mercado255@gmail.com>
2019-05-09 11:40:28 -0400

    adding animation and tuning turning and speed

--- EAEW3D/Assets/Scripts/Mover.cs
+++ EAEW3D/Assets/Scripts/Mover.cs
@@ -11,6 +11,8 @@ public class Mover : MonoBehaviour
         if (Input.GetMouseButtonDown(0)){
             MoveToCursor();
         }
+
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -21,6 +23,14 @@ public class Mover : MonoBehaviour
 
         if (hasHit){
             GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+        }    
     }
+    private void UpdateAnimator(){
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+
+}
+
 }
