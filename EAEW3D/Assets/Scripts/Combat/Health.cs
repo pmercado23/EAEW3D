a06738260c4670c53b498a9ab284749722bc86e6
Peter Mercado <peter.mercado255@gmail.com>
2019-05-10 15:06:50 -0400

    adding death animations

--- EAEW3D/Assets/Scripts/Combat/Health.cs
+++ EAEW3D/Assets/Scripts/Combat/Health.cs
@@ -6,12 +6,27 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthpoints = 100f;
+
+        bool isDead = false;
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthpoints = Mathf.Max(healthpoints - damage, 0);
+            if (healthpoints == 0)
+            {
+                Die();
+
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+
         }
     }
 }
