1164ac96d8b91625f52832401f2d809b2a840215
Peter Mercado <peter.mercado255@gmail.com>
2019-05-19 17:09:27 -0400

    finsihed fader and starting saving system

---
+++ EAEW3D/Assets/Scripts/Saving/SavingSystem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingSystem : MonoBehaviour
+    {
+        public void Save(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            print("saving to " + path);
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                Transform playerTransform = GetPlayerTransform();
+                byte[] buffer = SerializeVector(playerTransform.position);
+                stream.Write(buffer, 0, buffer.Length);
+            } 
+        }
+
+        public void Load(string saveFile)
+        {
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + path);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                Transform playerTransform = GetPlayerTransform();
+                playerTransform.position = DeserializeVector(buffer);
+
+            }
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorByte = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorByte, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorByte, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorByte, 8);
+            return vectorByte;
+        }
+
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
+        }
+
+        private string GetPathFromSaveFile(string savefile)
+        {
+            return Path.Combine(Application.persistentDataPath, savefile + ".sav");
+        }
+    }
+}
