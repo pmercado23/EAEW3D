7442ceb3f7d1f7da436d7b8ba4832cabb1bed67c
Peter Mercado <peter.mercado255@gmail.com>
2019-05-22 23:23:34 -0400

    adding arrows

--- EAEW3D/Assets/Scripts/Combat/Weapon.cs
+++ EAEW3D/Assets/Scripts/Combat/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using RPG.Core;
+using UnityEngine;
 
 namespace RPG.Combat
 {
@@ -11,14 +12,14 @@ namespace RPG.Combat
         [SerializeField] AnimatorOverrideController animatorOverride = null;
         [SerializeField] GameObject equippedPrefab = null;
         [SerializeField] bool isRightHanded = true;
+        [SerializeField] Projectile projectile = null;
+
 
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
             if (equippedPrefab != null)
             {
-                Transform handTransform;
-                if (isRightHanded) { handTransform = rightHand; }
-                else { handTransform = leftHand; }
+                Transform handTransform = GetTransform(rightHand, leftHand);
                 Instantiate(equippedPrefab, handTransform);
             }
 
@@ -26,6 +27,28 @@ namespace RPG.Combat
             {
                 animator.runtimeAnimatorController = animatorOverride;
             }
+        }
+
+        private Transform GetTransform(Transform rightHand, Transform leftHand)
+        {
+            Transform handTransform;
+            if (isRightHanded) { handTransform = rightHand; }
+            else { handTransform = leftHand; }
+
+            return handTransform;
+        }
+
+        public bool HasProjectile()
+        {
+            return projectile != null; 
+        }
+
+        public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target)
+        {
+            Projectile projectileInstance = Instantiate(projectile,
+                GetTransform(rightHand, leftHand).position, Quaternion.identity);
+
+            projectileInstance.SetTarget(target);
         }
 
         public float GetDamage()
