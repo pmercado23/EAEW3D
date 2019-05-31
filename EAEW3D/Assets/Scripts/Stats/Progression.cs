4a664a79820310f4d50e3a15a00c3a82e30b7d7c
unknown <peter.mercado255@gmail.com>
2019-05-31 15:05:47 -0400

    fixing goof

---
+++ EAEW3D/Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "RPG Project/Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+
+    }
+}
