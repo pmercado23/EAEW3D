50658c7147cb44df15f8966a9c764df1b4698385
unknown <peter.mercado255@gmail.com>
2019-06-07 11:24:24 -0400

    saving and displaying exp points

--- EAEW3D/Assets/Scripts/Resources/Experience.cs
+++ EAEW3D/Assets/Scripts/Resources/Experience.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0;
     
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public float GetExp()
+        {
+            return experiencePoints;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
+        
         }
     }
 }
